27ca21645e400eb58d44440dd33b883795989d2f
webulent <me@bulentkocaman.com>
2020-10-19 17:28:10 +0100

    Added curl request

--- src/Models/RS/AddRebate.php
+++ src/Models/RS/AddRebate.php
@@ -1,97 +1,51 @@
 <?php
 
-use Httpful\Response;
+namespace App\Models\RS;
 
-class approvdata{
+use App\Helpers\Database;
+use App\Helpers\Logger;
+use App\Models\AbstractModel;
 
-    
-   
+/**
+ * Class AddRebate
+ * @package App\Models
+ */
+class AddRebate extends AbstractModel
+{
+    public $response;
 
-   public $response;
-   public $id;
-
-   public function __construct()
-   {
-    $this->rdb =  Db::getInstance('recycling');
-      // $this->gdb =  Db::getInstance('greenoak');
-    
-  }
-  
- 
-
-    public function getlist(){
-
-        $sql = "SELECT username, firstname, lastname, email, approved, id as idnum  from recyc_users;";
-        $result = $this->rdb->prepare($sql);
-        $result->execute();
-      
-        $data = $result->fetchAll(PDO::FETCH_OBJ);
-
-        $dataarry = array();
-
-        foreach($data as $val){
-          array_push($dataarry, $val);
-        }
-        $this->response =json_encode($dataarry);
-                   return $this->response;
-
+    /**
+     * AddRebate constructor.
+     */
+    public function __construct()
+    {
+        $this->gdb = Database::getInstance('greenoak');
+        parent::__construct();
     }
 
-    public function updateapp(){
+    public function apicurlrequest($userid)
+    {
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://stoneapi.stonegroup.co.uk/stoneapp/rmApproval/" . $userid,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => array(),
+        ));
+        $output = curl_exec($curl);
 
-      $id = isset($_POST['idnum']) ? $_POST['idnum'] : '';
+        Logger::getInstance("responseAPI.log")->debug(
+            'apicurlrequest',
+            [$output]
+        );
 
-      if(!empty($id)){
-         
-          $this->apicurlrequest($id);
-
-          $sql = "UPDATE recyc_users
-          set approved = (case when approved = 'Y' then 'N' ELSE 'Y' END),
-          active = (case when active = 1 then 0 ELSE 1 END)
-          Where id = :id";
-          
-          $result = $this->rdb->prepare($sql);
-          $result->execute(array(':id' => $id));
-
-          return $this->response;
-      }
-   
+        return $output;
     }
-
-
-  public function apicurlrequest($userid){
-
-            // create & initialize a curl session
-      $curl = curl_init();
-
-
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://stoneapi.stonegroup.co.uk/stoneapp/rmApproval/".$userid,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_SSL_VERIFYPEER  => false,
-        CURLOPT_HTTPHEADER => array(),
-      ));
-      $output = curl_exec($curl);
-
-      $fp = fopen($_SERVER["DOCUMENT_ROOT"]."/RS_Files/responseAPI.txt", 'a+');
-      fwrite($fp, $output);
-      fclose($fp);
-
-
-      $this->response = $output;
-      return $this->response;
-    }
-
-
-
-
 }
-
-?>

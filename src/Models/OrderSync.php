04d1ca685108a54587fe51c05e52cab775ddb56a
Alex Smith <Alex.Smith@stonegroup.co.uk>
2020-11-11 17:04:06 +0000

    added update method to Ordersync

--- src/Models/OrderSync.php
+++ src/Models/OrderSync.php
@@ -1,9 +1,12 @@
 <?php
 
+
 namespace App\Models;
 
 use App\Helpers\Database;
 use App\Helpers\Logger;
+use Exception;
+
 
 /**
  * Class OrderSync
@@ -15,6 +18,7 @@ class OrderSync extends AbstractModel
     public $orders;
     public $companies;
     public $newOrders = [];
+    public $upOrders = [];
 
     /**
      * OrderSync constructor.
@@ -47,7 +51,7 @@ class OrderSync extends AbstractModel
 		WHERE
 			DELPROD.SalesOrderID IN
 				(Select SalesOrderId FROM SalesOrders WHERE CompanyId IN
-					(SELECT CompanyID FROM Company WHERE [CompanyDescription] LIKE N'%portal%')
+					(SELECT CompanyID FROM Company )
 				AND OrderPlacedDate > :from
 				)
 		";
@@ -67,22 +71,240 @@ class OrderSync extends AbstractModel
             );
         }
     }
-
-    public function process()
+    public function upprocess()
     {
-        Logger::getInstance("OrderSync.log")->info(
+        Logger::getInstance("OrderSync1.log")->info(
             'process',
             []
         );
-        foreach ($this->orders as $order) {
-            // first check order information
-            $sql = 'SELECT * FROM recyc_order_information WHERE sales_order_number = :order';
-            try {
-                $result = $this->rdb->prepare($sql);
-                $result->execute(array(':order' => $order->sales_order_number));
-                $exists = $result->fetch(\PDO::FETCH_OBJ);
 
-                if (!$exists) {
+    
+
+        foreach ($this->orders as $order) {
+
+            
+            // first check order information
+            //waste_transfer_number
+            $this->upOrders[$order->sales_order_number] = $order;
+           
+
+            Logger::getInstance("orderids.log")->info(
+                'order-ids',
+                [$order->sales_order_id]
+            );
+
+           
+
+                try {
+                    $sql = 'SELECT * FROM recyc_order_information WHERE sales_order_number = :order';
+                    $result = $this->rdb->prepare($sql);
+                    $result->execute(array(':order' => $order->sales_order_number));
+                    $exists = $result->fetch(\PDO::FETCH_OBJ);
+    
+    
+    
+    
+                    if(empty($order->waste_transfer_number)){
+    
+                        
+    
+                        $sql = "SELECT * from recyc_company_sync WHERE greenoak_id = :greenoak";
+                        try {
+                            $result = $this->rdb->prepare($sql);
+                            $result->execute(array(':greenoak' => $order->company_id));
+                            $order->company = $result->fetch(\PDO::FETCH_OBJ);
+                        } catch (\Exception $e) {
+                            Logger::getInstance("OrderSync2.log")->error(
+                                'process-error',
+                                [$e->getLine(), $e->getMessage()]
+                            );
+                        }
+    
+    
+                        $sql = "(
+                            SELECT
+                                Cast(SO.CompanyID AS NVARCHAR(max))  as 'company_id',
+                                Cast(SO.SalesOrderID AS NVARCHAR(max))  as 'sales_order_id',
+                                SO.SalesOrderNumber as 'sales_order_number',
+                                DEL.WasteTransferNumber as 'waste_transfer_number',
+                                A.LocationName  as 'location_name',
+                                Cast(DELLOC.PickupLocationID AS NVARCHAR(max)) as 'location_id',
+                                A.Address1 as 'address1',
+                                A.Address2 as 'address2',
+                                A.Address3 as 'address3',
+                                A.Address4 as 'address4',
+                                A.Town as 'town',
+                                A.PostCode as 'postcode',
+                                COALESCE(B.RegionName,'') AS 'county',
+                                C.CountryName AS 'country',
+                                A.TelNumber as 'telephone',
+                                A.SiteCode as 'sitecode',
+                                A.SICCode as 'sic_code',
+                                DEL.ActualDeliveryDate as 'actual_delivery_date'
+                            FROM
+                                [GREENOAK].[We3Recycler].[dbo].[SalesOrders] SO
+                            LEFT OUTER JOIN
+                                [GREENOAK].[We3Recycler].[dbo].Delivery DEL ON DEL.SalesOrderID = SO.SalesOrderID
+                            LEFT OUTER JOIN
+                                [GREENOAK].[We3Recycler].[dbo].DeliveryLocations DELLOC ON DEL.DeliveryID = DELLOC.DeliveryID
+                            LEFT OUTER JOIN
+                                [GREENOAK].[We3Recycler].[dbo].PickUpLocations A ON DELLOC.PickupLocationID = A.PickUpLocationID
+                            LEFT OUTER JOIN
+                                [GREENOAK].[We3Recycler].[dbo].Region B ON A.County = B.RegionID
+                            LEFT OUTER JOIN
+                                [GREENOAK].[We3Recycler].[dbo].Country C ON A.Country = C.CountryID
+                            WHERE
+                                DELLOC.PickUpLocationID NOT IN ('00000000-0000-0000-0000-000000000000')
+                                AND SO.SalesOrderID = :salesorderid2)
+                                UNION
+                                    (
+                                        SELECT
+                                          Cast(SO.CompanyID AS NVARCHAR(max)) as 'company_id',
+                                          Cast(SO.SalesOrderID AS NVARCHAR(max)) as 'sales_order_id',
+                                          SO.SalesOrderNumber as 'sales_order_number',
+                                          DEL.WasteTransferNumber as 'waste_transfer_number',
+                                          A.CompanyName as 'location_name',
+                                          Cast(DELLOC.PickupLocationID AS NVARCHAR(max)) as 'location_id',
+                                          A.PrimaryAddressLine1 AS address1,
+                                          A.PrimaryAddressLine2 AS address2,
+                                          A.PrimaryAddressLine3 AS address3,
+                                          A.PrimaryAddressLine4 AS address4,
+                                          A.PrimaryAddressTown AS town,
+                                          A.PrimaryAddressPostCode AS postcode,
+                                          COALESCE(B.RegionName,'') AS 'county',
+                                          C.CountryName AS 'country',
+                                          A.Telephone As telephone,
+                                          A.SiteCode as sitecode,
+                                          A.SICCode as sic_code,
+                                          DEL.ActualDeliveryDate as actual_delivery_date
+                                      FROM
+                                            [GREENOAK].[We3Recycler].[dbo].[SalesOrders] SO
+                                      LEFT OUTER JOIN
+                                            Company A ON SO.CompanyID = A.CompanyID
+                                      LEFT OUTER JOIN
+                                            Region B ON A.PrimaryAddressCounty = B.RegionID
+                                      LEFT OUTER JOIN
+                                            Country C ON A.PrimaryAddressCountry = C.CountryID
+                                      LEFT OUTER JOIN
+                                            Delivery DEL ON DEL.SalesOrderID = SO.SalesOrderID
+                                      LEFT OUTER JOIN
+                                            DeliveryLocations DELLOC ON DEL.DeliveryID = DELLOC.DeliveryID
+                                      LEFT OUTER JOIN
+                                            PickUpLocations D ON DELLOC.PickupLocationID = D.PickUpLocationID
+                                      WHERE
+                                            DELLOC.PickUpLocationID IN ('00000000-0000-0000-0000-000000000000'
+                                    )
+                                AND SO.SalesOrderID = :salesorderid
+                            )
+                            ";
+            
+                            try {
+                                $result = $this->gdb->prepare($sql);
+                                $result->execute(array(':salesorderid' => $order->sales_order_id, ':salesorderid2' => $order->sales_order_id));
+                                $order->data = $result->fetch(\PDO::FETCH_OBJ);
+                            } catch (\Exception $e) {
+                                Logger::getInstance("OrderSyncgreen.log")->error(
+                                    'Empty',
+                                    [$e->getLine(), $e->getMessage()]
+                                );
+                            }
+            
+                                $sql = 'UPDATE recyc_order_information
+                                set waste_transfer_number = :wtn,
+                                location_name = :locname,
+                                location_id = :locid, 
+                                address1 = :add1,
+                                address2 = :add2,
+                                address3 = :add3,
+                                address4 = :add4, 
+                                town = :town, 
+                                postcode = :postcode, 
+                                county = :county,
+                                country = :country,
+                                telephone = :phone,
+                                sitecode = :sitecode,
+                                sic_code = :siccode, 
+                                actual_delivery_date = :deldate
+                                where sales_order_number = :salesOrderNumber';
+            
+                                $orderInfo = array( 
+                                    ':salesOrderNumber' => $order->data->sales_order_number,
+                                    ':wtn' => $order->data->waste_transfer_number,
+                                    ':locname' => $order->data->location_name,
+                                    ':locid' => $order->data->location_id,
+                                    ':add1' => $order->data->address1,
+                                    ':add2' => $order->data->address2,
+                                    ':add3' => $order->data->address3,
+                                    ':add4' => $order->data->address4,
+                                    ':town' => $order->data->town,
+                                    ':postcode' => $order->data->postcode,
+                                    ':county' => $order->data->county,
+                                    ':country' => $order->data->country,
+                                    ':phone' => $order->data->telephone,
+                                    ':sitecode' => $order->data->sitecode,
+                                    ':siccode' => $order->data->sic_code,
+                                    ':deldate' => $order->data->actual_delivery_date
+                                );
+            
+                                $result = $this->rdb->prepare($sql);
+                                $result->execute($orderInfo);
+                            
+                            }
+                        }catch(Exception $e){
+                            Logger::getInstance("updateerrOrderSync.log")->error(
+                                'process-error',
+                                [$e->getLine(), $e->getMessage()]
+                            );
+                        }
+                    }
+        
+    }
+                        
+
+
+    public function process()
+    {
+        Logger::getInstance("OrderSync1.log")->info(
+            'process',
+            []
+        );
+
+    
+
+        foreach ($this->orders as $order) {
+
+            
+            // first check order information
+            //waste_transfer_number
+  
+           
+
+                try {
+                    $sql = 'SELECT * FROM recyc_order_information WHERE sales_order_number = :order';
+                    $result = $this->rdb->prepare($sql);
+                    $result->execute(array(':order' => $order->sales_order_number));
+                    $exists = $result->fetch(\PDO::FETCH_OBJ);
+    
+    
+    
+    
+                    if(empty($order->waste_transfer_number)){
+    
+                        
+    
+                        $sql = "SELECT * from recyc_company_sync WHERE greenoak_id = :greenoak";
+                        try {
+                            $result = $this->rdb->prepare($sql);
+                            $result->execute(array(':greenoak' => $order->company_id));
+                            $order->company = $result->fetch(\PDO::FETCH_OBJ);
+                        } catch (\Exception $e) {
+                            Logger::getInstance("OrderSync2.log")->error(
+                                'process-error',
+                                [$e->getLine(), $e->getMessage()]
+                            );
+                        }
+
+            } if (!$exists) {
                     $this->newOrders[$order->sales_order_number] = $order;
                 }
             } catch (\Exception $e) {
@@ -91,7 +313,7 @@ class OrderSync extends AbstractModel
                     [$e->getLine(), $e->getMessage()]
                 );
             }
-        }
+        
 
         if (!empty($this->newOrders)) {
             Logger::getInstance("OrderSync.log")->debug(
@@ -187,7 +409,7 @@ class OrderSync extends AbstractModel
 					[GREENOAK].[We3Recycler].[dbo].Country C ON A.Country = C.CountryID
 				WHERE
 					DELLOC.PickUpLocationID NOT IN ('00000000-0000-0000-0000-000000000000')
-					AND SO.SalesOrderID = :salesorderid)
+					AND SO.SalesOrderID = :salesorderid2)
 					UNION
 						(
 							SELECT
@@ -232,7 +454,7 @@ class OrderSync extends AbstractModel
 
                 try {
                     $result = $this->gdb->prepare($sql);
-                    $result->execute(array(':salesorderid' => $newOrder->sales_order_id));
+                    $result->execute(array(':salesorderid' => $newOrder->sales_order_id, ':salesorderid2' => $newOrder->sales_order_id));
                     $newOrder->data = $result->fetch(\PDO::FETCH_OBJ);
                 } catch (\Exception $e) {
                     Logger::getInstance("OrderSync.log")->error(
@@ -318,6 +540,7 @@ class OrderSync extends AbstractModel
                 ["There are no new orders to add."]
             );
             echo "There are no new orders to add.";
+            }
         }
     }
-}
+}   
